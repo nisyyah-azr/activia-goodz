7565e4760ec7fb06b3ba51d39f3e3910b5af5f36
nisyyah-azr <nisyyah.azzahra@ui.ac.id>
2025-09-17 01:44:33 +0700

    Perbaikan SCHEMA

--- activia_goodz/settings.py
+++ activia_goodz/settings.py
@@ -18,6 +18,8 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env.prod")
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
